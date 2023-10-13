6bb47af7d6a9b005da8e4de709a593e4b88f48f2
Zbigniew Lukasiak <zzbbyy@gmail.com>
2023-10-13 09:26:42 +0200

    Using a stopword.

--- react.py
+++ react.py
@@ -14,35 +14,35 @@ Solve a question answering task with interleaving Thought, Action, Observation s
 After each observation, provide the next Thought and next Action. Here are some examples:
 
 Question: What is the elevation range for the area that the eastern sector of the Colorado orogeny extends into?
-Thought 1: I need to search Colorado orogeny, find the area that the eastern sector of the Colorado orogeny extends into, then find the elevation range of the area.
-Action 1: Search[Colorado orogeny]
-Observation 1: The Colorado orogeny was an episode of mountain building (an orogeny) in Colorado and surrounding areas. This took place from 1780 to 1650 million years ago (Mya), during the Paleoproterozoic (Statherian Period). It is recorded in the Colorado orogen, a >500-km-wide belt of oceanic arc rock that extends southward into New Mexico. The Colorado orogeny was likely part of the larger Yavapai orogeny.
-Thought 2: It does not mention the eastern sector of the Colorado orogeny. I need to look up eastern sector.
-Action 2: Lookup[eastern sector]
-Observation 2: (Result 1 / 1) The Colorado orogen, formerly called the Colorado province, is a >500-km-wide belt of oceanic arc rock (1.78\u20131.65 Ga) that extends southward into New Mexico and composes a major part of the Proterozoic provinces of southwestern United States. This transcontinental collisional event occurred during the  Paleoproterozoic (Statherian Period).[1] The Wyoming sector of the Colorado orogeny was formerly called the Medicine Bow orogeny. The eastern sector extends into the High Plains and is called the Central Plains orogeny. The boundary between the Colorado orogeny and the Wyoming craton is the Cheyenne belt, a 5-km-wide mylonitic shear zone that verges northward. The Cheyenne belt transects and cuts off the south edge of the older Trans-Hudson orogeny.[2]
-Thought 3: The eastern sector of Colorado orogeny extends into the High Plains, so High Plains is the area. I need to search High Plains and find its elevation range.
-Action 3: Search[High Plains]Observation 3: High Plains refers to one of two distinct land regions:
-Thought 4: I need to search High Plains (United States).
-Action 4: Search[High Plains (United States)]
+Thought: I need to search Colorado orogeny, find the area that the eastern sector of the Colorado orogeny extends into, then find the elevation range of the area.
+Action: Search[Colorado orogeny]
+Observation: The Colorado orogeny was an episode of mountain building (an orogeny) in Colorado and surrounding areas. This took place from 1780 to 1650 million years ago (Mya), during the Paleoproterozoic (Statherian Period). It is recorded in the Colorado orogen, a >500-km-wide belt of oceanic arc rock that extends southward into New Mexico. The Colorado orogeny was likely part of the larger Yavapai orogeny.
+Thought: It does not mention the eastern sector of the Colorado orogeny. I need to look up eastern sector.
+Action: Lookup[eastern sector]
+Observation: (Result 1 / 1) The Colorado orogen, formerly called the Colorado province, is a >500-km-wide belt of oceanic arc rock (1.78\u20131.65 Ga) that extends southward into New Mexico and composes a major part of the Proterozoic provinces of southwestern United States. This transcontinental collisional event occurred during the  Paleoproterozoic (Statherian Period).[1] The Wyoming sector of the Colorado orogeny was formerly called the Medicine Bow orogeny. The eastern sector extends into the High Plains and is called the Central Plains orogeny. The boundary between the Colorado orogeny and the Wyoming craton is the Cheyenne belt, a 5-km-wide mylonitic shear zone that verges northward. The Cheyenne belt transects and cuts off the south edge of the older Trans-Hudson orogeny.[2]
+Thought: The eastern sector of Colorado orogeny extends into the High Plains, so High Plains is the area. I need to search High Plains and find its elevation range.
+Action: Search[High Plains]Observation 3: High Plains refers to one of two distinct land regions:
+Thought: I need to search High Plains (United States).
+Action: Search[High Plains (United States)]
 Observation 4: The High Plains are a subregion of the Great Plains, mainly in the Western United States, but also partly in the Midwest states of Nebraska, Kansas, and South Dakota, generally encompassing the western part of the Great Plains before the region reaches the Rocky Mountains. The High Plains are located in eastern Montana, southeastern Wyoming, southwestern South Dakota, western Nebraska, eastern Colorado, western Kansas, eastern New Mexico, western Oklahoma, and to just south of the Texas Panhandle.[4] The southern region of the Western High Plains ecology region contains the geological formation known as Llano Estacado which can be seen from a short distance or on satellite maps.[5] From east to west, the High Plains rise in elevation from around 1,800 to 7,000 ft (550 to 2,130 m).[3]
-Thought 5: High Plains rise in elevation from around 1,800 to 7,000 ft, so the answer is 1,800 to 7,000 ft.
-Action 5: Finish[1,800 to 7,000 ft]
+Thought: High Plains rise in elevation from around 1,800 to 7,000 ft, so the answer is 1,800 to 7,000 ft.
+Action: Finish[1,800 to 7,000 ft]
 
 Question: Musician and satirist Allie Goertz wrote a song about the "The Simpsons" character Milhouse, who Matt Groening named after who?
-Thought 1: The question simplifies to "The Simpsons" character Milhouse is named after who. I only need to search Milhouse and find who it is named after.
-Action 1: Search[Milhouse]
-Observation 1: Milhouse Mussolini Van Houten is a recurring character in the Fox animated television series The Simpsons voiced by Pamela Hayden and created by Matt Groening.
-Thought 2: The paragraph does not tell who Milhouse is named after, maybe I can look up "named after".
-Action 2: Lookup[named after]
-Observation 2: (Result 1 / 1) Milhouse was named after U.S. president Richard Nixon, whose middle name was Milhous. 
-Thought 3: Milhouse was named after U.S. president Richard Nixon, so the answer is Richard Nixon.
-Action 3: Finish[President Richard Nixon]
+Thought: The question simplifies to "The Simpsons" character Milhouse is named after who. I only need to search Milhouse and find who it is named after.
+Action: Search[Milhouse]
+Observation: Milhouse Mussolini Van Houten is a recurring character in the Fox animated television series The Simpsons voiced by Pamela Hayden and created by Matt Groening.
+Thought: The paragraph does not tell who Milhouse is named after, maybe I can look up "named after".
+Action: Lookup[named after]
+Observation: (Result 1 / 1) Milhouse was named after U.S. president Richard Nixon, whose middle name was Milhous. 
+Thought: Milhouse was named after U.S. president Richard Nixon, so the answer is Richard Nixon.
+Action: Finish[President Richard Nixon]
 
 {input}
 '''
 
 
-def openai_query(prompt, api_key):
+def openai_query(prompt, stops, api_key):
     headers = {
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json"
@@ -51,6 +51,7 @@ def openai_query(prompt, api_key):
         "model": "gpt-3.5-turbo-16k",
         "messages": [{ "role": "user", "content": prompt }],
         "temperature": 0.7,
+        "stop": stops,
     }
 
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
@@ -58,7 +59,7 @@ def openai_query(prompt, api_key):
         # TODO: maybe parse out the "Please try again in Xms" part and be clever
         print("Rate limit reached, waiting a bit...")
         time.sleep(5)
-        return openai_query(prompt, api_key)
+        return openai_query(prompt, stops, api_key)
     response_json = response.json()
     return response_json["choices"][0]["message"]["content"].strip()
 
@@ -70,12 +71,29 @@ with open("config.json", "r") as f:
 
 question = "What was the first major battle in the Ukrainian War?"
 
+def wikipedia_search(query):
+    return 'aaaaaaaaaaaaaaaaaaaaaaaaaa'
+
 done = False
+MAX_ITER = 2
+iter = 0
 while not done:
-   prompt = cot_prompt_short.format(input=question)
-   print(prompt)
-   reaction = openai_query(prompt, api_key)
-   print(reaction)
-   done = True
+    prompt = cot_prompt_short.format(input=question)
+    print(prompt)
+    reaction = openai_query(prompt, "\nObservation:", api_key)
+    print(reaction)
+    lines = reaction.strip().split('\n')
+    line = lines[-1]
+    if line.startswith("Action Finish["):
+        done = True
+    elif line.startswith("Action: Search["):
+        query = line[15:-1]
+        print(f'Will search wikipedia for "{query}"')
+        text = wikipedia_search(query)
+        print(text)
+        prompt = prompt + f'\nObservation: {text}\n'
+    if iter >= MAX_ITER:
+        done = True
+    iter += 1
 
 

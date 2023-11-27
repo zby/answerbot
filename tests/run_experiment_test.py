ab5e748ab7026c3e79999e4a52e284464cbe60ea
Zbigniew Lukasiak <zzbbyy@gmail.com>
2023-11-27 19:18:32 +0100

    test for evaling prompt

--- tests/run_experiment_test.py
+++ tests/run_experiment_test.py
@@ -1,16 +1,11 @@
 import unittest
 from unittest.mock import patch, Mock
 import os
-
-from scripts.run_experiment import perform_experiments
-import unittest
-from unittest.mock import patch, Mock
-import os
 import shutil
 
 # Assuming the refactored script with perform_experiments function is named "experiment_script.py"
 from scripts.run_experiment import  perform_experiments
-
+from prompt_builder import FunctionalPrompt
 
 class TestPerformExperiments(unittest.TestCase):
 
@@ -29,8 +24,7 @@ class TestPerformExperiments(unittest.TestCase):
     @patch('scripts.run_experiment.get_answer')  # This mocks the get_answer function
     def test_perform_experiments(self, mock_get_answer):
         # Create a mock prompt and set its to_text return value
-        mock_prompt = Mock()
-        mock_prompt.to_text.return_value = "Mocked Prompt Text"
+        mock_prompt = FunctionalPrompt([])
 
         # Set the return value of get_answer to a tuple with the mocked answer and the mock prompt
         mock_get_answer.return_value = ("Mocked Answer", mock_prompt)
@@ -52,16 +46,24 @@ class TestPerformExperiments(unittest.TestCase):
         self.assertTrue(os.path.exists(os.path.join(self.output_dir, "results.csv")))
         self.assertTrue(os.path.exists(os.path.join(self.output_dir, "prompts.txt")))
 
+        prompt_file = os.path.join(self.output_dir, "prompts", '0.txt')
+        self.assertTrue(os.path.exists(prompt_file))
+        file = open(prompt_file, 'r')
+        file_content = file.read()
+        file.close()
+        prompt = eval(file_content)
+        self.assertIsInstance(prompt, FunctionalPrompt)
+
         # Validate the content of prompts.txt
         expected_prompt_fragments = [
             "Question: Test Question\n",
             "Config: {'chunk_size': 300, 'prompt': 'FRP', 'example_chunk_size': 300, 'max_llm_calls': 5, 'model': 'gpt-3.5-turbo'}\n",
-            "Prompt:\nMocked Prompt Text\n"
         ]
-        with open(os.path.join(self.output_dir, "prompts.txt"), 'r') as file:
-            prompt_content = file.read()
-            for i in expected_prompt_fragments:
-                self.assertIn(i, prompt_content)
+        file = open(os.path.join(self.output_dir, "prompts.txt"), 'r')
+        prompt_content = file.read()
+        file.close()
+        for i in expected_prompt_fragments:
+            self.assertIn(i, prompt_content)
 
 
 if __name__ == "__main__":

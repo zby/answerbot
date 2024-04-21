dede15e2dc8db0967f61c5ac9ffcc39a986f1562
Zbigniew Lukasiak <zzbbyy@gmail.com>
2024-04-21 21:20:31 +0200

    test fixed

--- tests/prompt_templates_test.py
+++ tests/prompt_templates_test.py
@@ -4,12 +4,12 @@ def test_prompt():
     question = "What is your name?"
     max_llm_calls = 5
     reflection_class = ShortReflection
-    prompt = NoExamplesReactPrompt(question, max_llm_calls, reflection_class)
+    prompt = NoExamplesReactPrompt(question, max_llm_calls, reflection_class, False)
     system_message = prompt.to_messages()[0]
     assert "call shortreflection_and_read_chunk to retrieve" in system_message['content']
 
     reflection_class = None
-    prompt = NoExamplesReactPrompt(question, max_llm_calls, reflection_class)
+    prompt = NoExamplesReactPrompt(question, max_llm_calls, reflection_class, False)
     system_message = prompt.to_messages()[0]
     assert "call read_chunk to retrieve" in system_message['content']
 

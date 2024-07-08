e7be70079e564b178587ac8768f5f3c085ca1f76
Zbigniew Lukasiak <zzbbyy@gmail.com>
2024-07-08 04:47:02 +0200

    toolloop app extracted into a separate file

--- answerbot/chat.py
+++ answerbot/chat.py
@@ -3,31 +3,22 @@ from dataclasses import dataclass, asdict, field, is_dataclass
 from pydantic import BaseModel
 from litellm import completion, ModelResponse
 from jinja2 import Template
-from dotenv import load_dotenv
 
 from llm_easy_tools import get_tool_defs, process_response, ToolResult, LLMFunction 
 
 from answerbot.tools.wiki_tool import WikipediaTool
 
-import litellm
 import logging
-from pprint import pprint
-
-load_dotenv()
-litellm.success_callback = ["langfuse"]
-litellm.failure_callback = ["langfuse"]
-#litellm.success_callback=["helicone"]
-#litellm.set_verbose=True
-
 
 T = TypeVar('T')
 
-def render(template_str: str, obj: T, chat: Optional[Any] = None) -> str:
+def render_prompt(template_str: str, obj: T, context: Optional[object] = None) -> str:
     if not is_dataclass(obj):
         raise TypeError("The 'obj' parameter must be a dataclass instance")
     template = Template(template_str)
     fields = asdict(obj)
-    return template.render(chat=chat, **fields)
+    result = template.render(context=context, **fields)
+    return result
 
 @runtime_checkable
 class HasLLMTools(Protocol):
@@ -52,13 +43,20 @@ class Prompt:
 
 @dataclass
 class Chat:
+    model: str
     entries: list[Prompt|ToolResult|dict] = field(default_factory=list)
     templates: dict[Type[Prompt], str] = field(default_factory=dict)
     system_prompt: Optional[Prompt] = None
+    one_tool_per_step: bool = True
+    # With statefull tools with many tool calls the LLM gets confused about the state of the tools
+    # There should be an option in the litellm api for that
+    context: object = None
+    # for use in prompt templates
+
 
     def make_message(self, prompt: Prompt, role: str = 'user') -> str:
         template_str = prompt.template()
-        content = render(template_str, prompt, self)
+        content = render_prompt(template_str, prompt, self.context)
         return {
             'role': role,
             'content': content.strip()
@@ -78,21 +76,8 @@ class Chat:
                 messages.append(message)
         return messages
 
-    def user_question(self) -> Optional[str]:
-        for entry in self.entries:
-            if isinstance(entry, Question):
-                return entry.question
-        return None
-
-@dataclass
-class ChatProcessor:
-    model: str
-    one_tool_per_step: bool = True
-    # With statefull tools with many tool calls the LLM gets confused about the state of the tools
-    # There should be an option in the litellm api for that
-
-    def llm_reply(self, chat: Chat, schemas=[]) -> ModelResponse:
-        messages = chat.to_messages()
+    def llm_reply(self, schemas=[]) -> ModelResponse:
+        messages = self.to_messages()
         args = {
             'model': self.model,
             'messages': messages
@@ -118,122 +103,15 @@ class ChatProcessor:
             if not hasattr(message, 'tool_calls') or not message.tool_calls:
                 logging.warning(f"No function call:\n")
 
-        chat.entries.append(message)
+        self.entries.append(message)
 
         return result
 
-    def process(self, chat: Chat, tools: list[Callable|LLMFunction]):
+    def process(self, context: object, tools: list[Callable|LLMFunction]):
         schemas = get_tool_defs(tools)
-        response = self.llm_reply(chat, schemas)
+        response = self.llm_reply(schemas)
         results = process_response(response, tools)
         for result in results:
-            chat.entries.append(result)
+            self.entries.append(result)
+        return results
 
-@dataclass(frozen=True)
-class SystemPrompt(Prompt):
-    """
-    System prompt for the chat.
-    """
-
-    @classmethod
-    def template(cls) -> str:
-        return """You are a helpful assistant with extensive knowledge of wikipedia.
-You always try to support your answer with quotes from wikipedia.
-You remember that the information you receive from the wikipedia api is not the full page - it is just a fragment.
-You always try to answer the user question, even if it is ambiguous, just note the necessary assumptions.
-You Work carefully - never make two calls to wikipedia in the same step."""
-
-@dataclass(frozen=True)
-class Question(Prompt):
-    question: str
-    max_llm_calls: int
-
-    @classmethod
-    def template(cls) -> str:
-        return """Please answer the following question. You can use wikipedia for reference - but think carefully about what pages exist at wikipedia.
-You have only {{max_llm_calls}} calls to the wikipedia API.
-When searching wikipedia never make any complex queries, always decide what is the main topic you are searching for and put it in the search query.
-When you want to know a property of an object or person - first find the page of that object or person and then browse it to find the property you need.
-
-When you know the answer call Answer. Please make the answer as short as possible. If it can be answered with yes or no that is best.
-Remove all explanations from the answer and put them into the reasoning field.
-
-Question: {{question}}"""
-
-@dataclass
-class Answer:
-    """
-    Answer to the question.
-    """
-    answer: str
-    reasoning: str
-
-    @classmethod
-    def template(cls) -> str:
-        return """The answer to the question:"{{chat.user_question()}}" is:
-{{ answer }}
-
-Reasoning:
-{{ reasoning }}"""
-
-@dataclass(frozen=True)
-class StepInfo(Prompt):
-    step: int
-    max_steps: int
-
-    @classmethod
-    def template(cls) -> str:
-        return """
-Step: {{step + 1}} of {{max_steps + 1}}
-{% if step >= max_steps - 1 %}
-This was the last data retrieval in the next step you must provide an answer to the user question
-{% endif %}
-"""
-
-@dataclass
-class QAApp:
-    toolbox: list[HasLLMTools|LLMFunction|Callable]
-    max_iterations: int
-    model: str
-
-    def __post_init__(self):
-        self.step = 0
-        self._chat_processor = ChatProcessor(model=self.model, one_tool_per_step=True)
-        self.chat = Chat(system_prompt=SystemPrompt())
-
-    def get_tools(self) -> list[Callable|LLMFunction]:
-        tools = [Answer]
-        if self.step < self.max_iterations:
-            tools.extend(expand_toolbox(self.toolbox))
-        return tools
-
-    def process(self, question: str):
-        chat = self.chat
-        chat.entries.append(Question(question, self.max_iterations))
-        while(self.step <= self.max_iterations):
-            tools = self.get_tools()
-            self._chat_processor.process(chat, tools)
-            result = chat.entries[-1]
-            if result.error:
-                raise Exception(result.error)
-            if result.soft_errors:
-                for soft_error in result.soft_errors:
-                    logging.warning(soft_error)
-            if isinstance(result.output, Answer):
-                answer = result.output
-                return render(Answer.template(), answer, chat)
-            chat.entries.append(StepInfo(self.step, self.max_iterations))
-            self.step += 1
-        return None
-
-model='claude-3-5-sonnet-20240620'
-#model='gpt-3.5-turbo'
-
-app = QAApp(
-    toolbox=[WikipediaTool(chunk_size=400)],
-    max_iterations=5,
-    model=model,
-)
-
-question = "What government position was held by the woman who portrayed Corliss Archer in the film Kiss and Tell?"
-print(app.process(question))
---
+++ answerbot/qa_processor.py
@@ -0,0 +1,134 @@
+from dataclasses import dataclass
+from answerbot.chat import Chat, Prompt, HasLLMTools, expand_toolbox, render_prompt
+from answerbot.tools.wiki_tool import WikipediaTool 
+from typing import Callable
+
+from llm_easy_tools import LLMFunction
+
+import logging
+import litellm
+from dotenv import load_dotenv
+
+#from pprint import pprint
+
+load_dotenv()
+litellm.success_callback = ["langfuse"]
+litellm.failure_callback = ["langfuse"]
+#litellm.success_callback=["helicone"]
+#litellm.set_verbose=True
+
+
+@dataclass(frozen=True)
+class SystemPrompt(Prompt):
+    """
+    System prompt for the chat.
+    """
+
+    @classmethod
+    def template(cls) -> str:
+        return """You are a helpful assistant with extensive knowledge of wikipedia.
+You always try to support your answer with quotes from wikipedia.
+You remember that the information you receive from the wikipedia api is not the full page - it is just a fragment.
+You always try to answer the user question, even if it is ambiguous, just note the necessary assumptions.
+You Work carefully - never make two calls to wikipedia in the same step."""
+
+@dataclass(frozen=True)
+class Question(Prompt):
+    question: str
+    max_llm_calls: int
+
+    @classmethod
+    def template(cls) -> str:
+        return """Please answer the following question. You can use wikipedia for reference - but think carefully about what pages exist at wikipedia.
+You have only {{max_llm_calls}} calls to the wikipedia API.
+When searching wikipedia never make any complex queries, always decide what is the main topic you are searching for and put it in the search query.
+When you want to know a property of an object or person - first find the page of that object or person and then browse it to find the property you need.
+
+When you know the answer call Answer. Please make the answer as short as possible. If it can be answered with yes or no that is best.
+Remove all explanations from the answer and put them into the reasoning field.
+
+Question: {{question}}"""
+
+@dataclass
+class Answer:
+    """
+    Answer to the question.
+    """
+    answer: str
+    reasoning: str
+
+    @classmethod
+    def template(cls) -> str:
+        return """The answer to the question:"{{context.question}}" is:
+{{ answer }}
+
+Reasoning:
+{{ reasoning }}"""
+
+@dataclass(frozen=True)
+class StepInfo(Prompt):
+    step: int
+    max_steps: int
+
+    @classmethod
+    def template(cls) -> str:
+        return """
+Step: {{step + 1}} of {{max_steps + 1}}
+{% if step >= max_steps - 1 %}
+This was the last data retrieval in the next step you must provide an answer to the user question
+{% endif %}
+"""
+
+@dataclass
+class QAApp:
+    toolbox: list[HasLLMTools|LLMFunction|Callable]
+    max_iterations: int
+    model: str
+    question: str
+
+    def __post_init__(self):
+        self.step = 0
+        self.chat = Chat(model=self.model, one_tool_per_step=True, system_prompt=SystemPrompt(), context=self)
+
+    def get_tools(self) -> list[Callable|LLMFunction]:
+        tools = [Answer]
+        if self.step < self.max_iterations:
+            tools.extend(expand_toolbox(self.toolbox))
+        return tools
+
+    def process(self):
+        chat = self.chat
+        chat.entries.append(Question(self.question, self.max_iterations))
+        while(self.step <= self.max_iterations):
+            tools = self.get_tools()
+            self.chat.process(self, tools)
+            result = chat.entries[-1]
+            if result.error:
+                raise Exception(result.error)
+            if result.soft_errors:
+                for soft_error in result.soft_errors:
+                    logging.warning(soft_error)
+            if isinstance(result.output, Answer):
+                answer = result.output
+                return render_prompt(Answer.template(), answer, self)
+            chat.entries.append(StepInfo(self.step, self.max_iterations))
+            self.step += 1
+        return None
+
+
+model='claude-3-5-sonnet-20240620'
+#model='gpt-3.5-turbo'
+question = "What government position was held by the woman who portrayed Corliss Archer in the film Kiss and Tell?"
+
+app = QAApp(
+    toolbox=[WikipediaTool(chunk_size=400)],
+    max_iterations=5,
+    model=model,
+    question=question
+)
+
+#answer = Answer(answer="Something", reasoning="Because")
+#print(render_prompt(Answer.template(), answer, app))
+#print(app.question)
+
+print(app.process())

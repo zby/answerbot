3b8b2156fb7c2bf0f2ac9ccb7c8b701020745568
Zbigniew Lukasiak <zzbbyy@gmail.com>
2024-07-26 18:02:04 +0200

    relevant_quotes and new_sources made optional - otherwise we get errors

--- answerbot/reflection_result.py
+++ answerbot/reflection_result.py
@@ -36,8 +36,8 @@ def find_similar_fragments(text, quote):
 class ReflectionResult(BaseModel):
     what_have_we_learned: Optional[str] = Field('', description="Have we learned anything that would help us answer the user question from the retrieved information and why?")
     comment: str = Field(..., description="A comment on the retrieved information.")
-    relevant_quotes: list[str] = Field(..., description="A list of relevant literal quotes from the source that should be saved.")
-    new_sources: list[str] = Field(..., description="A list of new urls mentioned in the notes that should be checked later.")
+    relevant_quotes: Optional[list[str]] = Field(default_factory=list, description="A list of relevant literal quotes from the source that should be saved.")
+    new_sources: Optional[list[str]] = Field(default_factory=list, description="A list of new urls mentioned in the notes that should be checked later.")
 
     @field_validator('new_sources', mode='before')
     def unique_new_sources(cls, v):
